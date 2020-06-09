52eb1fa3fdb511eb308ba4610dbec80a25b79989
Maksim <maksim903926@gmail.com>
2020-06-09 22:26:55 +0100

    3-rd stage

---
+++ Hangman/task/hangman/hangman.py
@@ -0,0 +1,12 @@
+import random
+
+
+print("H A N G M A N")
+
+word_list = ['python', 'java', 'kotlin', 'javascript']
+random.seed()
+right_answer = random.choice(word_list)
+if input("Guess the word: > ") == right_answer:
+    print("You survived!")
+else:
+    print("You are hanged!")
