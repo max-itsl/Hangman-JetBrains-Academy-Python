40844f2a3751513251fc4760187b09034d229d17
Maksim <maksim903926@gmail.com>
2020-06-09 22:35:06 +0100

    5-th stage

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,16 +1,25 @@
 import random
 
 
-print("H A N G M A N")
+print("H A N G M A N\n")
 
 word_list = ['python', 'java', 'kotlin', 'javascript']
 random.seed()
 right_answer = random.choice(word_list)
-if input("Guess the word "
-         + right_answer[:3]
-         + "-" * (len(right_answer) - 3)
-         + ": > "
-         ) == right_answer:
-    print("You survived!")
-else:
-    print("You are hanged!")
+
+secret_word = "-" * len(right_answer)
+
+for i in range(8):
+    print(secret_word)
+    char = input("Input a letter: ")
+    if char not in right_answer:
+        print("No such letter in the word")
+    else:
+        for j in range(len(right_answer)):
+            if right_answer[j] == char:
+                list1 = list(secret_word)
+                list1[j] = char
+                secret_word = "".join(list1)
+    print()
+
+print("Thanks for playing!\nWe'll see how well you did in the next stage")
