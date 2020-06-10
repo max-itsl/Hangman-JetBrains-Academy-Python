f282d6619a2cec9b2143ebaceca9c123951a63de
Maksim <maksim903926@gmail.com>
2020-06-10 20:30:00 +0100

    7-th stage

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -14,23 +14,28 @@ tried_chars = set()
 while lives > 0:
     print(secret_word)
     if right_answer == secret_word:
-        print("You guessed the word!\nYou survived!")
+        print(f"You guessed the word {right_answer}!\nYou survived!")
         break
     char = input("Input a letter: ")
-    if char not in tried_chars:
-        tried_chars.add(char)
-        if char not in right_answer:
-            lives -= 1
-            print("No such letter in the word")
+    if 0 < len(char) < 2:
+        if char.islower() and char.isalpha():
+            if char in tried_chars:
+                print("You already typed this letter")
+            else:
+                tried_chars.add(char)
+                if char not in right_answer:
+                    lives -= 1
+                    print("No such letter in the word")
+                else:
+                    for j in range(len(right_answer)):
+                        if right_answer[j] == char:
+                            temp_list = list(secret_word)
+                            temp_list[j] = char
+                            secret_word = "".join(temp_list)
         else:
-            for j in range(len(right_answer)):
-                if right_answer[j] == char:
-                    temp_list = list(secret_word)
-                    temp_list[j] = char
-                    secret_word = "".join(temp_list)
+            print("It is not an ASCII lowercase letter")
     else:
-        lives -= 1
-        print("No improvements")
+        print("You should input a single letter")
     print()
 else:
     print("You are hanged!")
