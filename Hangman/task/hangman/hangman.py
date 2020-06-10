acbb2287474986ade074ef677325aed9239a28b3
Maksim <maksim903926@gmail.com>
2020-06-10 21:02:14 +0100

    8-th stage

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -11,31 +11,38 @@ secret_word = "-" * len(right_answer)
 lives = 8
 tried_chars = set()
 
-while lives > 0:
-    print(secret_word)
-    if right_answer == secret_word:
-        print(f"You guessed the word {right_answer}!\nYou survived!")
-        break
-    char = input("Input a letter: ")
-    if 0 < len(char) < 2:
-        if char.islower() and char.isalpha():
-            if char in tried_chars:
-                print("You already typed this letter")
-            else:
-                tried_chars.add(char)
-                if char not in right_answer:
-                    lives -= 1
-                    print("No such letter in the word")
+while True:
+    game = input('Type "play" to play the game, "exit" to quit:')
+    if game == "play":
+        while lives > 0:
+            print(secret_word)
+            if right_answer == secret_word:
+                print(f"You guessed the word {right_answer}!\nYou survived!")
+                break
+            char = input("Input a letter: ")
+            if 0 < len(char) < 2:
+                if char.islower() and char.isalpha():
+                    if char in tried_chars:
+                        print("You already typed this letter")
+                    else:
+                        tried_chars.add(char)
+                        if char not in right_answer:
+                            lives -= 1
+                            print("No such letter in the word")
+                        else:
+                            for j in range(len(right_answer)):
+                                if right_answer[j] == char:
+                                    temp_list = list(secret_word)
+                                    temp_list[j] = char
+                                    secret_word = "".join(temp_list)
                 else:
-                    for j in range(len(right_answer)):
-                        if right_answer[j] == char:
-                            temp_list = list(secret_word)
-                            temp_list[j] = char
-                            secret_word = "".join(temp_list)
+                    print("It is not an ASCII lowercase letter")
+            else:
+                print("You should input a single letter")
+            print()
         else:
-            print("It is not an ASCII lowercase letter")
+            print("You are hanged!")
+    elif game == "exit":
+        break
     else:
-        print("You should input a single letter")
-    print()
-else:
-    print("You are hanged!")
+        continue
