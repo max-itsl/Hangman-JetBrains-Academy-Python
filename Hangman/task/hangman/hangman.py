f657fceda33db35bd44685694f68fb3c5a982de4
Maksim <maksim903926@gmail.com>
2020-06-10 19:20:56 +0100

    6-th stage

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -8,18 +8,29 @@ random.seed()
 right_answer = random.choice(word_list)
 
 secret_word = "-" * len(right_answer)
+lives = 8
+tried_chars = set()
 
-for i in range(8):
+while lives > 0:
     print(secret_word)
+    if right_answer == secret_word:
+        print("You guessed the word!\nYou survived!")
+        break
     char = input("Input a letter: ")
-    if char not in right_answer:
-        print("No such letter in the word")
+    if char not in tried_chars:
+        tried_chars.add(char)
+        if char not in right_answer:
+            lives -= 1
+            print("No such letter in the word")
+        else:
+            for j in range(len(right_answer)):
+                if right_answer[j] == char:
+                    temp_list = list(secret_word)
+                    temp_list[j] = char
+                    secret_word = "".join(temp_list)
     else:
-        for j in range(len(right_answer)):
-            if right_answer[j] == char:
-                list1 = list(secret_word)
-                list1[j] = char
-                secret_word = "".join(list1)
+        lives -= 1
+        print("No improvements")
     print()
-
-print("Thanks for playing!\nWe'll see how well you did in the next stage")
+else:
+    print("You are hanged!")
